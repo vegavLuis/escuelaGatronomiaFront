ff07fc12e4e1477e801ed506fedf516c5dfb04bf
Luis Vega <70670285+070809-vega@users.noreply.github.com>
2023-07-29 20:28:59 -0600

    se corrigen errores

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,13 +7,11 @@
 
 <script>
 import Carousel from "../components/Carousel.vue";
-import Licenciatura from '@/components/Licenciatura.vue';
 export default {
   name: "Home",
 
   components: {
     Carousel,
-    Licenciatura,
   },
 };
 </script>
